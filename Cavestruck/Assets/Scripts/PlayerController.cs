1054a59c0fa798fcc798c602a30a52ddda1471a3
santiagosteam5 <facsonnmartens@gmail.com>
2025-04-19 17:24:34 -0500

    Movement done, jump with buffer

--- Cavestruck/Assets/Scripts/PlayerController.cs
+++ Cavestruck/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    public float speed = 5f;
-    public float jumppower = 5f;
+    public float speed = 6;
+    public float jumppower = 6f;
+    private bool isGrounded = true;
+    private float jumpBufferTime = 0.2f;
+    private float lastJumpTime = -1f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.mass = 1f;
+        rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
     }
 
 
@@ -25,20 +32,43 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
-        movementY = movementVector.y;
+        Debug.Log($"MovementX: {movementX}");
     }
 
     private void jumpBro()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            lastJumpTime = Time.time;
+        }
+
+        if(isGrounded && Time.time - lastJumpTime <= jumpBufferTime)
+        {
             rb.AddForce(Vector3.up * jumppower, ForceMode.Impulse);
+            isGrounded = false;
+            lastJumpTime = -1f;
         }
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, movementY, 0.0f);
-        rb.AddForce(movement * speed);
+        Vector3 targetVelocity = new Vector3(movementX * speed, rb.linearVelocity.y, 0.0f);
+        rb.linearVelocity = targetVelocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
